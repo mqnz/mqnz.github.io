046c76d5f727c9b32e32f94689e228ce4b9bd7df
Makkenzi <pahovich@gmail.com>
2017-05-31 12:57:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bootstrap сайт</title>
+	<link rel="stylesheet" href"css/bootstrap.css">
+	<link rel="stylesheet" href"css/font-awesome.min.css">
+	<link rel="stylesheet" href"css/main.css">
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed=top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>	
+			</div>
+		</div>
+	<div>	
+</body>
+</html>	
